2023bb41b22127165ed8e03f94b0e7030cdcac44
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-29 23:43:52 +0000

    LIFO Caching system

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""LIFO Caching"""
+
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """A simple LIFO caching system"""
+    def __init__(self):
+        """Class initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """add an item in the cache"""
+        if not key or key not in self.cache_data.keys():
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discared = self.cache_data.popitem()
+            print(f"DISCARD: {discared}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get a cache by its key"""
+        if not key or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
