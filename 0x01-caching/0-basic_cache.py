417b934b6b8426fa8edc7868f95a139728cc1990
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-29 20:52:23 +0000

    Basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,9 +2,21 @@
 """Basic dictionary"""
 
 
-BaseCaching = __import__("base_caching").BaseCaching
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """A caching system"""
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if not key or not item:
+            return
+        self.cache_data[key] = item
 
+    def get(self, key):
+        """ Get an item by key
+        """
+        if not key or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
