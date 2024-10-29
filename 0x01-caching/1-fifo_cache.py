9f81469b1bb5df748d87737c6210affeea97e56c
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-29 21:38:07 +0000

    FIFO caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""FIFO caching"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Class of FIFO caching system"""
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if not key or not item:
+            return
+
+        if self.MAX_ITEMS < len(self.cache_data):
+            (key := next(iter(self.cache_data)), self.cache_data.pop(key))
+            print(f"DISCARD: {key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if not key or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
