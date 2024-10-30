5157a7171a5cabcb4928504f8a0d8fdea57af04a
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-30 15:35:06 +0000

    LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""LRU Csching"""
+
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Least Recently Used (LRU) Caching system"""
+    def __init__(self):
+        """Initialize the class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item to cache"""
+        if not key or not item:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded = self.cache_data.popitem()
+            print(f"DISCARD: {discarded[0]}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item from cache by it's key"""
+        if not key or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
