6154e88e6821a12ac50ff05dd221508de799fb02
Brehyma Traore <brehymatraore50@gmail.com>
2024-11-06 14:50:19 +0000

    Type hint added

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,10 +8,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'], strict_slashes=False)
+@app.route("/", methods=["GET", "POST"], strict_slashes=False)
 def home() -> str:
     return render_template("0-index.html")
- 
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
