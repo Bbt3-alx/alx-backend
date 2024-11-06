e27a26cd79c5aea49b9df994db3c876f26b8ed1f
Brehyma Traore <brehymatraore50@gmail.com>
2024-11-06 15:13:47 +0000

    Updated

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,26 +2,27 @@
 """Basic Babel setup"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
+
+
+app = Flask(__name__)
 
 
 class Config:
+    """App configuration"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
-# app.config.from_pyfile('mysettings.cfg')
-app.config.from_object(Config)
-babel = Babel(app)
-app.config["BABEL_DEFAULT_LOCALE"] = "fr"
-
-
-@babel.localeselector
 def get_locale():
     """Get locale"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+# app.config.from_pyfile('mysettings.cfg')
+app.config.from_object(Config)
+babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route("/")
